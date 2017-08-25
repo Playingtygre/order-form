013422d9d6e05540715bf5d6aadfb3699fbd91ee
stariel <ariel.altaras@gmail.com>
2017-08-25 10:25:41 -0700

    got qty to populate in object

--- cart.js
+++ cart.js
@@ -5,7 +5,6 @@ var orderArray = JSON.parse(localStorage.getItem('orders'));
 var orderSection = document.getElementById('ordersSection');
 for (var i = 0; i < orderArray.length; i++) {
   var orderSpot = document.createElement('div');
-  orderSpot.setAttribute('orderIndex', i);
   orderSection.appendChild(orderSpot);
   var pic = document.createElement('img');
   pic.src = orderArray[i].imgPath;
@@ -15,21 +14,21 @@ for (var i = 0; i < orderArray.length; i++) {
   newP.innerHTML = orderArray[i].name + ', ' + orderArray[i].addy + ', ' + orderArray[i].phone;
   orderSpot.appendChild(newP);
   var qtyP = document.createElement('p');
-  qtyP.innerText = orderArray[i].qty;
+  qtyP.innerText = 'QTY: ' + orderArray[i].qty;
   orderSpot.appendChild(qtyP);
   var button = document.createElement('button');
-  button.innerHTML = '<img src="img/delete.png" class="button">Delete this item';
+  button.innerHTML = '<img src="img/delete.png" class="button"/>Delete this item';
+  button.id = i;
+  button.addEventListener('click', delOrder);
   orderSpot.appendChild(button);
   var clear = document.createElement('div');
   clear.className = 'clearFix';
   orderSpot.appendChild(clear);
 }
-var delButton = document.getElementsByClassName('button');
-delButton.addEventListener('click', delOrder);
 
 function delOrder(event) {
-  var orderIndex = parseInt(event.target.getAttribute('orderIndex'));
-  var clickedOrder = orderArray[orderIndex];
-  orderArray.splice(index, orderIndex);
+  var orderIndex = event.target.id;
+  orderArray.splice(orderIndex, 1);
   localStorage.setItem('orders', JSON.stringify(orderArray));
+  location.reload();
 };
